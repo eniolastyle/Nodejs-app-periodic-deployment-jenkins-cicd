c83e7dc32945abe6ce02c85ed0a8a85130dcb790
eniolastyle <amiolastyle@gmail.com>
2023-07-19 18:15:21 +0100

    integration test added successfully

--- test.js
+++ test.js
@@ -1,161 +1,113 @@
-var express = require('express');
-var app = express();
-var request = require('supertest');
-var mongoose = require('mongoose');
-var Todo = require('./models/todo.js');
-var router = require('./routes/todo.js');
+const request = require('supertest');
+const app = require('./server.js');
+const mongoose = require('mongoose');
+const { expect } = require('chai');
+const Todo = require('./models/todo.js');
 
-// Use the router for the '/todos' endpoint
-app.use('/todos', router);
+describe('Todo API', () => {
+  // Create sample data before running the tests
+  before(async () => {
+    await mongoose.connect(
+      'mongodb+srv://goadmin:2JUjHAIk1ON46itZ@godatabase.pi7lxgz.mongodb.net/',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
 
-// Connect to a test database
-before(function (done) {
-  // Increase timeout to 5000ms
+    // Clear existing data in the test database
+    await Todo.deleteMany({});
 
-  var mongoDB = 'mongodb+srv://yusufolatuji53:LEi65TxnW3yLTprz@memories.wtf14jp.mongodb.net/test';
-  mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }, function (err) {
-    if (err) {
-      console.error('Error connecting to test database:', err);
-      done(err);
-    } else {
-      done();
-    }
-  });
-});
-
-// Clear the test database after each test
-afterEach(function (done) {
-  Todo.deleteMany({}, function (err) {
-    if (err) {
-      console.error('Error clearing test database:', err);
-      done(err);
-    } else {
-      done();
-    }
-  });
-});
-
-// Close the database connection after all tests are done
-after(function (done) {
-  mongoose.connection.close(function (err) {
-    if (err) {
-      console.error('Error closing test database connection:', err);
-      done(err);
-    } else {
-      done();
-    }
-  });
-});
-
-describe('Todo API', function () {
-  describe('GET /todos', function () {
-    it('should retrieve all todos', function (done) {
-      // Create some sample todos in the test database
-      var sampleTodos = [
-        { text: 'Todo 1' },
-        { text: 'Todo 2' },
-      ];
-      Todo.create(sampleTodos, function (err) {
-        if (err) return done(err);
-        // Make a GET request to the /todos endpoint
-        request(app)
-          .get('/todos')
-          .expect(200)
-          .expect(function (res) {
-            if (res.body.length !== 2) {
-              throw new Error('Incorrect number of todos');
-            }
-            if (res.body[0].text !== 'Todo 1' || res.body[1].text !== 'Todo 2') {
-              throw new Error('Incorrect todo data');
-            }
-          })
-          .end(done);
-      });
-    });
+    // Insert sample data into the test database
+    await Todo.insertMany([{ text: 'Todo 1' }, { text: 'Todo 2' }]);
   });
 
-  describe('POST /todos', function () {
-    it('should create a new todo', function (done) {
-      // Make a POST request to the /todos endpoint with a new todo
+  // Clean up the test database after running the tests
+  after(async () => {
+    // Remove all data from the test database
+    await Todo.deleteMany({});
+    // Disconnect from the test database
+    await mongoose.connection.close();
+  });
+
+  // Test GET /api/todos
+  describe('GET /api/todos', () => {
+    it('should retrieve all todos', (done) => {
       request(app)
-        .post('/todos')
-        .send({ text: 'New Todo' })
-        .expect(201)
-        .expect(function (res) {
-          if (res.body.text !== 'New Todo') {
-            throw new Error('Incorrect todo data');
-          }
-        })
-        .end(function (err) {
+        .get('/api/todos')
+        .expect(200)
+        .end((err, res) => {
           if (err) return done(err);
-          // Check if the todo is saved in the test database
-          Todo.find({}, function (err, todos) {
-            if (err) return done(err);
-            if (todos.length !== 1 || todos[0].text !== 'New Todo') {
-              return done(new Error('Todo not saved in database'));
-            }
-            done();
-          });
+
+          // Assertions
+          expect(res.body.length).to.equal(2);
+          expect(res.body[0].text).equal('Todo 1');
+          expect(res.body[1].text).to.equal('Todo 2');
+
+          done();
         });
     });
   });
 
-  describe('PUT /todos/:id', function () {
-    it('should update a todo', function (done) {
-      // Create a sample todo in the test database
-      Todo.create({ text: 'Sample Todo' }, function (err, todo) {
-        if (err) return done(err);
-        // Make a PUT request to update the todo's text
-        request(app)
-          .put('/todos/' + todo._id)
-          .send({ text: 'Updated Todo' })
-          .expect(200)
-          .expect(function (res) {
-            if (res.body.text !== 'Updated Todo') {
-              throw new Error('Incorrect todo data');
-            }
-          })
-          .end(function (err) {
-            if (err) return done(err);
-            // Check if the todo is updated in the test database
-            Todo.findById(todo._id, function (err, updatedTodo) {
-              if (err) return done(err);
-              if (!updatedTodo || updatedTodo.text !== 'Updated Todo') {
-                return done(new Error('Todo not updated in database'));
-              }
-              done();
-            });
-          });
-      });
+  // Test POST /api/todos
+  describe('POST /api/todos', () => {
+    it('should create a new todo', (done) => {
+      request(app)
+        .post('/api/todos')
+        .send({ text: 'New Todo' })
+        .expect(201)
+        .end(async (err, res) => {
+          if (err) return done(err);
+
+          // Assertions
+          expect(res.body.text).to.equal('New Todo');
+
+          // Check if the new todo exists in the database
+          const todos = await Todo.find();
+          expect(todos.length).to.equal(3);
+          expect(todos[2].text).to.equal('New Todo');
+
+          done();
+        });
     });
   });
 
-  describe('DELETE /todos/:id', function () {
-    it('should delete a todo', function (done) {
-      // Create a sample todo in the test database
-      Todo.create({ text: 'Sample Todo' }, function (err, todo) {
-        if (err) return done(err);
-        // Make a DELETE request to delete the todo
-        request(app)
-          .delete('/todos/' + todo._id)
-          .expect(200)
-          .expect(function (res) {
-            if (res.body.message !== 'Todo deleted') {
-              throw new Error('Incorrect response message');
-            }
-          })
-          .end(function (err) {
-            if (err) return done(err);
-            // Check if the todo is deleted from the test database
-            Todo.findById(todo._id, function (err, deletedTodo) {
-              if (err) return done(err);
-              if (deletedTodo) {
-                return done(new Error('Todo not deleted from database'));
-              }
-              done();
-            });
-          });
-      });
+  // Test PUT /api/todos/:id
+  describe('PUT /api/todos/:id', () => {
+    it('should update a todo', async () => {
+      // Get the first todo from the database
+      const todos = await Todo.find();
+      const todoId = todos[0]._id;
+
+      const res = await request(app)
+        .put(`/api/todos/${todoId}`)
+        .send({ text: 'Updated Todo' });
+
+      // Assertions
+      expect(res.status).to.equal(200);
+      expect(res.body.text).to.equal('Updated Todo');
+
+      // Check if the todo is updated in the database
+      const updatedTodo = await Todo.findById(todoId);
+      expect(updatedTodo.text).to.equal('Updated Todo');
+    });
+  });
+
+  // Test DELETE /api/todos/:id
+  describe('DELETE /api/todos/:id', () => {
+    it('should delete a todo', async () => {
+      // Get the first todo from the database
+      const todos = await Todo.find();
+      const todoId = todos[0]._id;
+
+      const res = await request(app).delete(`/api/todos/${todoId}`).expect(200);
+
+      // Assertions
+      expect(res.body.message).to.equal('Todo deleted');
+
+      // Check if the todo is deleted from the database
+      const deletedTodo = await Todo.findById(todoId);
+      expect(deletedTodo).to.be.null;
     });
   });
 });
